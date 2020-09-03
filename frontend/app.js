8050a40b8982ad188439d9a489565a79f0cbc904
xu <1308937690@qq.com>
2020-09-03 23:56:58 +0800

    “增添标签页”

--- frontend/app.js
+++ frontend/app.js
@@ -1,39 +1,47 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
+    var that = this;
+    //1、调用微信登录接口，获取code
     wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+      success: function (r) {
+        var code = r.code;//登录凭证
+        if (code) {
+          //2、调用获取用户信息接口
           wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+            success: function (res) {
+              //3.请求自己的服务器，解密用户信息
+              wx.request({
+                url: 'http://127.0.0.1:8000/app1/index',
+                method: 'post',
+                header: {
+                  'content-type': "application/x-www-form-urlencoded"
+                },
+                data: { encryptedData: res.encryptedData, iv: res.iv, code: code },
+                success: function (res) {
+                  console.log(res.data);
+                  that.globalData.userInfo=res.data;
+                },
+                fail: function () {
+                  console.log('系统错误')
+                }
+              })
+            },
+            fail: function () {
+              console.log('获取用户信息失败')
             }
           })
+
+        } else {
+          console.log('获取用户登录态失败！' + r.errMsg)
         }
+      },
+      fail: function () {
+        console.log('登陆失败')
       }
     })
   },
   globalData: {
     userInfo: null
-  }
+  },
 })
---
+++ frontend/pages/label/label.js
@@ -0,0 +1,90 @@
+const app = getApp()
+Page({
+  data:{
+    albumName:'',
+    checked:false,
+    state:"",
+    showID:"",
+    inputValue:"",
+    label:[],
+    obtnArry:[
+      {
+        name:"信息学院",
+        num:0,
+        selected:false,
+      },
+      {
+        name:"数学学院",
+        num:1,
+        selected:false,
+      },
+      {
+        name:"环境学院",
+        num:1,
+        selected:false,
+      },
+      {
+        name:"理学院",
+        num:1,
+        selected:false,
+      },
+      {
+        name:"高瓴人工智能学院",
+        num:1,
+        selected:false,
+      },
+      {
+        name:"经济学院",
+        num:1,
+        selected:false,
+      },
+      {
+        name:"法学院",
+        num:1,
+        selected:false,
+      },
+      {
+        name:"社会人口学院",
+        num:1,
+        selected:false,
+      },
+    ]
+  },
+// 选择标签并输入到label数组中
+  dealTap:function(e){  
+    let string = "obtnArry[" + e.target.dataset.index + "].selected";
+    // const checkedicon = "obtnArry[" + e.target.dataset.index + "].selected"; 
+    console.log(!this.data.obtnArry[e.target.dataset.index].selected);
+    this.setData({
+      [string]: !this.data.obtnArry[e.target.dataset.index].selected
+    })
+    let detailValue = this.data.obtnArry.filter(it => it.selected).map(it => it.name)
+    this.setData({
+      label: detailValue
+    })
+    console.log(this.data.label)
+  },
+  //返回用户日志
+  comfirm:function(e){
+    var util = require('../../utils/util.js'); //获取时间
+    var m=app.globalData.userInfo  //获取全局向量
+    wx.request({
+      url: 'test.php', //访问地址
+      data: {
+        user_id: m['openId'],
+        user_name:m['nickName'],
+        user_tags:this.data.label,  //list
+        user_create_time:util.formatTime(new Date())     //格式：2020/09/03 22:56:19
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success (res) {
+        console.log(res.data)
+      }
+    })
+
+  }
+
+  
+})
